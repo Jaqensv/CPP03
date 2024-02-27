48a6ffa3c643ba3acf6747528d2eaba9643dee53
Matthieu De Langhe <mde-lang@k0r2p1.42mulhouse.fr>
2024-02-27 17:24:14 +0100

    ex00

---
+++ ex00/ClapTrap.class.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.class.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 14:33:48 by mde-lang          #+#    #+#             */
+/*   Updated: 2024/02/27 17:19:42 by mde-lang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.class.hpp"
+
+ClapTrap::ClapTrap() : _hp(10), _energy(10), _damage(0) {
+    std::cout << "basic ClapTrap constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : _hp(10), _energy(10), _damage(0), _name(name) {
+    std::cout << "ClapTrap " << name <<" constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+    std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+void ClapTrap::setName(std::string name) {
+    if (name.length() == 0)
+    {
+        this->_name = "Boris";
+        return ;
+    }
+    this->_name = name;
+}
+
+void ClapTrap::attack(const std::string& target) {
+    
+    if (this->_hp <= 0 || this->_energy <= 0)
+    {
+        std::cout << "ClapTrap " << this->_name << " is KO!" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->_name << " attacks " << target << " causing " << this->_damage << " points of damage!" << std::endl;
+    this->_energy--;
+    
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+    this->_hp -= amount;
+    std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+    if (this->_hp <= 0 || this->_energy <= 0)
+    {
+        std::cout << "ClapTrap " << this->_name << " is KO!" << std::endl;
+        return ;
+    }
+    this->_energy--;
+    this->_hp += amount;
+    std::cout << "ClapTrap " << this->_name << " get repaired and gains " << amount << " hp" << std::endl;
+}
+
+int ClapTrap::getHp() {
+    return _hp;
+}
---
+++ ex00/ClapTrap.class.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.class.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 14:33:45 by mde-lang          #+#    #+#             */
+/*   Updated: 2024/02/27 17:17:02 by mde-lang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_CLASS_HPP
+# define CLAPTRAP_CLASS_HPP
+
+# include <iostream>
+
+class ClapTrap {
+public:
+    ClapTrap();
+    ~ClapTrap();
+    ClapTrap(std::string name);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    void setName(std::string name);
+    int getHp();
+private:
+    std::string _name;
+    int _hp;
+    int _energy;
+    int _damage;
+};
+
+#endif
